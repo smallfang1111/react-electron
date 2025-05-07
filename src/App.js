6e572241d1c6f930fe61cd97250af9b7c42afa7f
v_lfangtian <v_lfangtian@tencent.com>
2025-05-07 19:57:08 +0800

    界面初始化

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+// 自定义左侧容器
+const LeftDiv = styled.div.attrs({
+  className: 'col-3 left-panel'
+})`
+background-color: #7b8c7c;
+min-height:100vh;
+`
+
+// 自定义右侧容器
+const RightDiv = styled.div.attrs({
+  className: 'col-9 right-panel'
+})`
+
+background-color: #ccc;
+`
+
 function App() {
   return (
-    <div className="App">
-小芳芳变厉害变有钱
+    <div className="App container-fluid">
+      <div className="row no-gutters">
+        <LeftDiv>左侧</LeftDiv>
+        <RightDiv>12侧</RightDiv>
+      </div>
     </div>
   );
 }
